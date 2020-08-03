0ff9cc4c61e599a18d68465b3483d08cc3b19d91
nos-nart <nosnart1510@gmail.com>
2020-08-03 08:21:14 +0700

    [+] ll

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const secrets = require('./secrets')
 const connectDB = require('./db')
 const logger = require('./logger')
 const { cloudinary } = require('cloudinary')
-const { upload } = require('./multer')
+const upload = require('./upload')
 
 module.exports = {
   secrets,
